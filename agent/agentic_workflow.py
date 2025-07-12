be4c385a28f58dd7947601610cddbedbea1a54eb
HasnainTariq1 <hasnaintariq172@gmail.com>
2025-07-13 02:30:44 +0500

    code added for loading model

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,24 @@
+from utils.model_loaders import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessageGraph, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+# from tools.weather_tool import WeaterInfoTool
+# from tools.search_place_tool import PlaceSearchTool
+# from tools.currency_conversion_tool import CurrencyConverterTool
+# from tools.expense_calculator_tool import CalculatorTool
+
+class GraphBuilder():
+
+    def __init__(self):
+        pass
+
+
+    def agent_function(self):
+        pass
+
+    def build_graph(self):
+        pass
+
+    def __call__(self):
+        pass
